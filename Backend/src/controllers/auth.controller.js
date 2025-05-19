38ffe753f3ce585d7b825b44314b84177294a55b
Aditya Kumar <adityakr123muz@gmail.com>
2025-05-19 17:12:23 +0530

    Added  login controller

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -20,12 +20,12 @@ export const register = async (req, res) => {
             })
         }
 
-        const hashedPassword = bcrypt.hash(password, 10);
+        const hashedPassword = await bcrypt.hash(password, 10);
 
         const newUser =  await db.user.create({
             data:{
                 email,
-                password:"" + hashedPassword,
+                password: hashedPassword,
                 name,
                 role: UserRole.USER
 
@@ -65,7 +65,60 @@ export const register = async (req, res) => {
     }
 }
 
-export const login = async () => {}
+export const login = async (req, res) => {
+
+    const {email, password} = req.body;
+
+    try {
+        const user = await db.user.findUnique({
+            where: {
+                email
+            }
+
+        })
+        if (!user) {
+            return res.status(400).json({
+                error: "user not found"
+            })
+        }
+
+
+        const ismatch = await bcrypt.compare(password, user.password);
+        console.log(ismatch);
+        if (!ismatch) {
+            return res.status(401).json({
+                error: "invalid credentials"
+            })
+        }
+        const token = jwt.sign({id:user.id}, process.env.JWT_SECRET, {
+            expiresIn: "7d"
+        })
+        res.cookie("jwt", token, {
+            httpOnly: true,
+            sameSite: "strict",
+            secure: process.env.NODE_ENV !== "development",
+            maxAge: 1000 * 60 * 60 * 24 * 7
+        })
+        res.status(200).json({
+            message: "login successfully",
+            user: {
+                id: user.id,
+                email: user.email,
+                name: user.name,
+                role: user.role,
+                image: user.image
+            }
+        })
+        
+    } catch (error) {
+        console.error("error logging in user", error);
+        res.status(500).json({
+            error: "logging in user error"
+        })
+        
+    }
+}
+
 
 export const logout = async () => {}
 
