bc4a1dd7a09d1a71f3e4e2e65d668233b79abf75
Aditya Kumar <adityakr123muz@gmail.com>
2025-07-18 13:02:31 +0530

    Added page's

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,38 @@
 import { useState } from 'react'
-
+import {Routes, Route, Navigate} from 'react-router-dom'
+import HomePage from './page/HomePage'
+import SignUpPage from './page/SignUpPage'
+import LoginPage from './page/LoginPage'
 
 
 function App() {
+  let authUser = null;
   
 
   return (
     <>
-      <div className='text-3xl text-white'>Hardcode.com</div>
+     <div className='flex flex-col items-center justify-center text-4xl'>
+
+     <Routes>
+      <Route
+      path = '/'
+      element = {authUser ? <HomePage/> : <Navigate to = '/login'/>}
+      />
+
+      <Route
+      path = '/signup'
+      element = {!authUser ? <SignUpPage/> : <Navigate to =  '/'/>}
+      />
+
+     <Route
+     path ='/login'
+     element = {!authUser ? <LoginPage/> : <Navigate to = '/' /> }
+     />
+     
+     
+     </Routes>
+
+     </div>
     </>
   )
 }
---
+++ Frontend/src/page/SignUpPage.jsx
@@ -0,0 +1,12 @@
+import React, { useState} from 'react'
+import {useForm} from 'react-hook-form'
+import {zoodResolver} from '@hookform/resolvers/zod'
+import {Link} from 'react-router-dom'
+
+const SignUpPage = () => {
+  return (
+    <div>SignUpPage</div>
+  )
+}
+
+export default SignUpPage
