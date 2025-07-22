0d5b87f8a92e800e63051fc697afb18df56a1502
Aditya Kumar <adityakr123muz@gmail.com>
2025-07-22 17:12:23 +0530

    Added cors

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieparser from "cookie-parser";
+import cors from "cors"
 
 import authRoutes from "./routes/auth.route.js";
 import problemRoutes from "./routes/problem.route.js";
@@ -12,7 +13,13 @@ dotenv.config();
 
 const app = express()
 
+app.use(cors ({
+   origin : "http://localhost:5173",
+   credentials: true
 
+}));
+  
+ 
 app.use(express.json());
 app.use(cookieparser());
 
